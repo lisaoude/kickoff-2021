fac01199742ef7306776a8b1cb559b072d17e611
Lisa Oude Elferink <lisaoudeelferink@hotmail.com>
2021-02-01 16:52:06 +0100

    css update

--- js/app.js
+++ js/app.js
@@ -1,23 +1,23 @@
-const xhr = new XMLHttpRequest()
-
-const url = 'https://600ff44f6c21e1001704fac2.mockapi.io/minor-web/api/squads/1/teams/1/members';
-
-const userData = {
-    "teamId": "1",
-    "name": "Lisa",
-    "prefix": "",
-    "surname": "Oude Elferink",
-    "mugshot": "https://avatars.githubusercontent.com/u/57795294?s=460&u=264667545ecf35ed8216f8d64effbad302a43494&v=4",
-    "githubHandle": "lisaoude",
-    "other": {
-        "age": "21",
-        "sport": "slapen",
-        "pet": "poes Kyra, kater Bram",
-        "muziek": "Lo-Fi, Pop, van alles",
-        "werkplek": "Thuis"
-    }
-}
-
-xhr.open("POST", url, true)
-xhr.setRequestHeader('Content-Type', 'application/json')
-xhr.send(JSON.stringify(userData))
+//const xhr = new XMLHttpRequest()
+//
+//const url = 'https://600ff44f6c21e1001704fac2.mockapi.io/minor-web/api/squads/1/teams/1/members/48';
+//
+//const userData = {
+//    "teamId": "1",
+//    "name": "Lisa",
+//    "prefix": "",
+//    "surname": "Oude Elferink",
+//    "mugshot": "https://avatars.githubusercontent.com/u/57795294?s=460&u=264667545ecf35ed8216f8d64effbad302a43494&v=4",
+//    "githubHandle": "lisaoude",
+//    "other": {
+//        "age": "21",
+//        "sport": "slapen",
+//        "pet": "poes Kyra, kater Bram",
+//        "muziek": "Lo-Fi, Pop, van alles",
+//        "werkplek": "Thuis"
+//    }
+//}
+//
+//xhr.open("DELETE", url, true)
+//xhr.setRequestHeader('Content-Type', 'application/json')
+//xhr.send(JSON.stringify(userData))
